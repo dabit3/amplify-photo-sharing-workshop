b62e5f1e9b39f2856511842a00d12b2b9c5ac1a9
Drew Smith <asmithvp@amazon.com>
2021-07-09 23:48:41 -0500

    Updated dependencies and @emotion/css

--- postagram/src/Post.js
+++ postagram/src/Post.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { css } from 'emotion';
+import { css } from '@emotion/css';
 import { useParams } from 'react-router-dom';
 import { API, Storage } from 'aws-amplify';
 import { getPost } from './graphql/queries';
